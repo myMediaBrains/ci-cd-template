35e5a2224910f47ceedaa9f4c65ffd702941242d
“myMediaBrains” <my.media.brains@gmail.com>
2022-07-14 23:00:47 +0000

    3rd commit

---
+++ src/app.test.js
@@ -0,0 +1,12 @@
+const app = require("./app")
+
+const supertest = require("supertest")
+const request = supertest(app);
+
+describe("/test endpoint", () => {
+  it("should retrune a response", async () => {
+    const response = await request.get("/test");
+    expect(response.status).toBe(200);
+    expect(response.text).toBe("Hello World");
+  })
+})
